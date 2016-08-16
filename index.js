58cfde527d68cf3a787aa84d465059fabd009847
David R. Poindexter III <drpoindexter@gmail.com>
2016-08-16 16:42:17 -0400

    Add nodemon for dev server file watching

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello World');
-});
+app.get('/', (req,res) => res.send('Hello World!'));
+app.get('/yo', (req,res) => res.send('YO!'));
+app.get('/:word', (req, res) => res.send(req.params.word));
 
 const server = app.listen(3000, () => console.log(`Server running at http://localhost:${server.address().port}`));
