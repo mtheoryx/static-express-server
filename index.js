30fea5cbedaa516ddb4773956dfc4d26c7dc3c69
David R. Poindexter III <drpoindexter@gmail.com>
2016-08-16 20:14:22 -0400

    Add ability to use handlebars or pug for templates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const _ = require('lodash');
 const fs = require('fs');
 const pug = require('pug');
+const engines = require('consolidate');
 
 const app = express();
 
@@ -20,11 +21,15 @@ fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
     });
 });
 
+app.engine('hbs', engines.handlebars);
+
 app.set('views', './views');
-app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => res.render('index', { users: users }));
 
-app.get('/:username', (req, res) => res.send(`Hello, ${req.params.username}`));
+app.get('/:username', (req, res) => {
+    res.render('user.pug', { user: users.find(user => user.username === req.params.username) })
+});
 
 const server = app.listen(3000, () => console.log(`Server running at http://localhost:${server.address().port}`));
